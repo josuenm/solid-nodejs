44c8cbc7c926a06fbcf5f8ab6531100bdd0e8191
josuenm <direct.josue@gmail.com>
2022-09-01 13:32:51 -0300

    style: execute function

--- src/modules/user/useCases/loginUser/LoginUserUseCase.ts
+++ src/modules/user/useCases/loginUser/LoginUserUseCase.ts
@@ -4,7 +4,19 @@ import { UserDTO } from "../../types/User";
 class LoginUserUseCase {
   constructor(private userRepository: IUserRepository) {}
 
-  execute({ email, password }: Omit<UserDTO, "id" | "name">) {}
+  async execute({ email, password }: Omit<UserDTO, "id" | "name">) {
+    const findUser = await this.userRepository.findUser(email);
+
+    if (findUser === "User not found") {
+      throw new Error("User not found");
+    }
+
+    const isPasswordCorrect = this.userRepository.login(password);
+
+    if (!isPasswordCorrect) {
+      throw new Error("Password did not match");
+    }
+  }
 }
 
 export { LoginUserUseCase };
