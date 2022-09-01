ee82ebd1eac003db4254118e1bd076a5e355531a
josuenm <direct.josue@gmail.com>
2022-09-01 13:28:16 -0300

    style: return a string promise instead a void promise

--- src/modules/user/repositories/implementations/IUserRepository.ts
+++ src/modules/user/repositories/implementations/IUserRepository.ts
@@ -2,7 +2,7 @@ import { UserDTO } from "../../types/User";
 
 export interface IUserRepository {
   create: (user: Omit<UserDTO, "id">) => Promise<void>;
-  findUser: (email: string) => Promise<void>;
+  findUser: (email: string) => Promise<string>;
   save: () => Promise<void>;
   login: (password: string) => boolean;
 }
