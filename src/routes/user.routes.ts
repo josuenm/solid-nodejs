caede1a5951499e5a03811e7afb790d204a471fe
josuenm <direct.josue@gmail.com>
2022-09-01 13:25:47 -0300

    feat: async/await added

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,10 +1,15 @@
 import express from "express";
 import { createUserController } from "../modules/user/useCases/createUser";
+import { loginUserController } from "../modules/user/useCases/loginUser";
 
 const userRoute = express.Router();
 
-userRoute.post("/create", (req, res) => {
-  return createUserController.handle(req, res);
+userRoute.post("/create", async (req, res) => {
+  return await createUserController.handle(req, res);
+});
+
+userRoute.post("/login", async (req, res) => {
+  return await loginUserController.handle(req, res);
 });
 
 export { userRoute };
