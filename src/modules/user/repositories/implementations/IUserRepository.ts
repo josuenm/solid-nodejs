13942aa3a98dfea79d8c9e23cd7ba7b556e52bd6
josuenm <direct.josue@gmail.com>
2022-09-01 12:43:26 -0300

    feat: new function

--- src/modules/user/repositories/implementations/IUserRepository.ts
+++ src/modules/user/repositories/implementations/IUserRepository.ts
@@ -4,4 +4,5 @@ export interface IUserRepository {
   create: (user: Omit<UserDTO, "id">) => Promise<void>;
   findUser: (email: string) => Promise<void>;
   save: () => Promise<void>;
+  login: (password: string) => boolean;
 }
