24cb617c7e584b2aa4623373eb014ccc57a1b406
josuenm <direct.josue@gmail.com>
2022-09-01 12:42:16 -0300

    feat: constructor requires repository

--- src/modules/user/useCases/loginUser/LoginUserUseCase.ts
+++ src/modules/user/useCases/loginUser/LoginUserUseCase.ts
@@ -1,7 +1,10 @@
+import { IUserRepository } from "../../repositories/implementations/IUserRepository";
 import { UserDTO } from "../../types/User";
 
 class LoginUserUseCase {
-  execute({ email, password }: Omit<UserDTO, "name">) {}
+  constructor(private userRepository: IUserRepository) {}
+
+  execute({ email, password }: Omit<UserDTO, "id" | "name">) {}
 }
 
 export { LoginUserUseCase };
