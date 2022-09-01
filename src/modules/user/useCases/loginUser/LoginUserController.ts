0e4ed02a80c759d42f3c234b6d6c90721e4e9e8a
josuenm <direct.josue@gmail.com>
2022-09-01 13:33:32 -0300

    feat: async/await added, switch/case added

--- src/modules/user/useCases/loginUser/LoginUserController.ts
+++ src/modules/user/useCases/loginUser/LoginUserController.ts
@@ -4,12 +4,28 @@ import { LoginUserUseCase } from "./LoginUserUseCase";
 class LoginUserController {
   constructor(private loginUserUseCase: LoginUserUseCase) {}
 
-  handle(request: Request, response: Response): void {
+  async handle(request: Request, response: Response): Promise<void> {
     const { email, password } = request.body;
 
-    this.loginUserUseCase.execute({ email, password });
+    try {
+      await this.loginUserUseCase.execute({ email, password });
 
-    response.status(201).send();
+      response.status(201).send();
+    } catch (error: any) {
+      switch (error.message) {
+        case "User not found":
+          response.status(401).json({ error: error.message });
+          break;
+
+        case "Password did not match":
+          response.status(401).json({ error: error.message });
+          break;
+
+        default:
+          response.status(500).json({ error: "Error server" });
+          break;
+      }
+    }
   }
 }
 
