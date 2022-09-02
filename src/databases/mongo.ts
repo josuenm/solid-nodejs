85c84c475872e69d0d44e830d928d1c125bc3b5e
josuenm <direct.josue@gmail.com>
2022-09-02 15:26:17 -0300

    style: function renamed
    connectToDatabase to connectToMongoDB

--- src/databases/mongo.ts
+++ src/databases/mongo.ts
@@ -1,10 +1,10 @@
 import mongoose from "mongoose";
 
-const connectToDatabase = async () => {
+const connectToMongoDB = async () => {
   await mongoose
     .connect("mongodb://localhost:27017/solid")
     .then(() => console.log("Connected to MongoDB"))
     .catch(() => console.log("Error trying to connect to MongoDB"));
 };
 
-export { connectToDatabase };
+export { connectToMongoDB };
