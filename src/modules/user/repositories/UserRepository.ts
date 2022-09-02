3ae850e88613273cfa6b4298444c9f12ede775dd
josuenm <direct.josue@gmail.com>
2022-09-02 16:31:58 -0300

    style: using postgresql

--- src/modules/user/repositories/UserRepository.ts
+++ src/modules/user/repositories/UserRepository.ts
@@ -1,4 +1,4 @@
-import { v4 as uuidv4 } from "uuid";
+import { client } from "../../../databases/postgre";
 import User from "../models/User";
 import { UserDTO } from "../types/User";
 import { IUserRepository } from "./implementations/IUserRepository";
@@ -29,7 +29,6 @@ class UserRepository implements IUserRepository {
 
   async create({ name, email, password }: Omit<UserDTO, "id">): Promise<void> {
     this.user = {
-      id: uuidv4(),
       name,
       email,
       password,
@@ -37,14 +36,24 @@ class UserRepository implements IUserRepository {
   }
 
   async save(): Promise<void> {
-    const user = new User(this.user);
-    await user.save();
+    const user = new User(this.user as UserDTO);
+
+    console.log(user);
+
+    await client
+      .query(
+        `INSERT INTO users(id, name, email, password) values('${user.id}', '${user.name}', '${user.email}', '${user.password})'`
+      )
+      .then((res) => console.log(res))
+      .catch((err) => console.log(err));
   }
 
   async findUser(email: string): Promise<string> {
-    const user = await User.findOne({ email });
+    const user: any = await client.query(
+      `SELECT * FROM users WHERE email='${email}'`
+    );
 
-    if (!user) {
+    if (user.rows.length === 0) {
       return "User not found";
     }
 
@@ -53,7 +62,7 @@ class UserRepository implements IUserRepository {
       name: user?.name,
       email: user?.email,
       password: user?.password,
-    } as UserDTO;
+    };
 
     return "User found";
   }
